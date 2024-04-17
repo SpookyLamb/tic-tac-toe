d054886837d773ac2e09b6d53d5cd7c71d992c54
SpookyLamb <turtleytyrant@gmail.com>
2024-04-17 10:01:12 -0400

    Initial commit.

---
+++ main.js
@@ -0,0 +1,28 @@
+// Must Have:
+// Game tiles should only be clickable once, and if the game is over they should not be clickable at all.
+// The game should display who wins the game if someone wins, otherwise, say that the game resulted in a tie (cat).  A win happens when one player has 3 symbols in a straight line (horizontal, vertical, or diagonal).
+// On each player’s turn, that player places their symbol on an unoccupied space by clicking.
+
+// Should Have:
+// The game should let the players know who's turn it is.
+// There should be a Restart Game button that does not refresh the page (set state).
+
+// Could Have: 
+// Allow players to enter their names
+// Keep track of the number of games won by X and O
+// Save information in local storage
+// Add a link to rules, display in a modal
+// Selectable themes - different board, background, and alternatives to X/O displays
+// Only have a single <div id="app"></div> in your index.HTML (try to code golf the HTML file)
+// Randomize who goes first
+// Display multiple game boards at once
+
+// Wish List:
+// Turn the project into Connect Four
+// Recreate Go
+
+function init() {
+
+}
+
+init()
